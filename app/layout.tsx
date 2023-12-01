8091b10d6a34a94ca2324b4c96944f883ce99254
master2it <mahdavi2it@gmail.com>
2023-12-01 17:17:52 +0300

    init google ads

--- app/layout.tsx
+++ app/layout.tsx
@@ -41,7 +41,6 @@ export default function RootLayout({
         <Script async
                 src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5595968868369218"
                 crossOrigin="anonymous"></Script>
-        <!-- Google tag (gtag.js) -->
         <Script async src="https://www.googletagmanager.com/gtag/js?id=G-P0TKX7R77S"></Script>
         <Script>
             window.dataLayer = window.dataLayer || [];
