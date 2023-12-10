298a9df7f13912b4e11a41ea63b13eebac1f2bcb
master2it <mahdavi2it@gmail.com>
2023-12-10 23:48:16 +0300

    generateMetadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,50 +1,51 @@
-import type {Metadata} from "next";
-import {Montserrat} from "next/font/google";
+import type { Metadata } from "next";
+import { Montserrat } from "next/font/google";
 import "./globals.css";
 import React from "react";
 import Provider from "../components/Provider";
-import {Analytics} from '@vercel/analytics/react';
+import { Analytics } from "@vercel/analytics/react";
 import Script from "next/script";
 
-const montserrat = Montserrat({subsets: ["latin"]});
+const montserrat = Montserrat({ subsets: ["latin"] });
 
 export const metadata: Metadata = {
-    title: "FiFi Movie",
-    description: "Watch and explore online with FiFi Movie",
+  title: "FiFi Movie | Watch and explore online",
+  description: "Watch and explore online with FiFi Movie",
 };
 
 declare global {
-    interface Window {
-        dataLayer: Record<string, any>[];
-    }
+  interface Window {
+    dataLayer: Record<string, any>[];
+  }
 }
 
 export default function RootLayout({
-                                       children,
-                                   }: {
-    children: React.ReactNode;
+  children,
+}: {
+  children: React.ReactNode;
 }) {
-    return (
-        <html lang="en" data-theme="mytheme">
-        <head>
-            <meta charSet="utf-8"/>
-            <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-            <meta name="google-adsense-account" content="ca-pub-5595968868369218"/>
-        </head>
-        <body className={montserrat.className}>
-        <Provider>
-            {children}
-        </Provider>
-        <Analytics/>
+  return (
+    <html lang="en" data-theme="mytheme">
+      <head>
+        <meta charSet="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="google-adsense-account" content="ca-pub-5595968868369218" />
+      </head>
+      <body className={montserrat.className}>
+        <Provider>{children}</Provider>
+        <Analytics />
 
-        <Script async
-                src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5595968868369218"
-                crossOrigin="anonymous"></Script>
-        <Script>
-            (adsbygoogle = window.adsbygoogle || []).push({});
-        </Script>
-        <Script async src="https://www.googletagmanager.com/gtag/js?id=G-P0TKX7R77S"></Script>
-        </body>
-        </html>
-    );
+        <Script
+          async
+          src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5595968868369218"
+          crossOrigin="anonymous"
+        ></Script>
+        <Script>(adsbygoogle = window.adsbygoogle || []).push({});</Script>
+        <Script
+          async
+          src="https://www.googletagmanager.com/gtag/js?id=G-P0TKX7R77S"
+        ></Script>
+      </body>
+    </html>
+  );
 }
--- app/movies/[id]/page.tsx
+++ app/movies/[id]/page.tsx
@@ -1,29 +1,33 @@
-"use client";
-
-import {useParams} from "next/navigation";
-import {useEffect, useState} from "react";
 import MovieCard from "@components/movies/movieCard";
 
-const MovieDetails = () => {
-    const params = useParams();
-    const [movie, setMovie]: any = useState({});
+export async function generateMetadata({ params }) {
+  const movie = await fetch(
+    `${process.env.BASE_URL}/movies/api/${params.id}`
+  ).then((res) => res.json());
 
-    useEffect(() => {
-        const fetchData = async () => {
-            const res = await fetch(`/movies/api/${params.id}`);
-            setMovie(await res.json());
-        };
-        fetchData();
-    }, []);
-    return (
-        <>
-            {Object.keys(movie).length ? (
-                <MovieCard movie={movie}/>
-            ) : (
-                <div>Loading...</div>
-            )}
-        </>
-    );
+  return {
+    title: movie.title_english + " | FiFi Movie",
+    desciption: movie.title_long,
+    generator: "FiFi Movie",
+    applicationName: "FiFi Movie",
+    referrer: "origin-when-cross-origin",
+    keywords: [...movie.genres, ...movie.slug.split("-")],
+  };
+}
+
+const MovieDetails = async ({ params }) => {
+  const movie = await fetch(
+    `${process.env.BASE_URL}/movies/api/${params.id}`
+  ).then((res) => res.json());
+  return (
+    <>
+      {movie && Object.keys(movie).length ? (
+        <MovieCard movie={movie} />
+      ) : (
+        <div>Loading...</div>
+      )}
+    </>
+  );
 };
 
 export default MovieDetails;
